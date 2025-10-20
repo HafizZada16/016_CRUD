7348984d2e5203d540c2b8ed11f5d44ff4aa8867
HafizZada16 <hafizzada16@gmail.com>
2025-10-20 12:46:07 +0700

    Membuat PUT api/users

--- index.js
+++ index.js
@@ -44,16 +44,34 @@ app.post('api/users', (req, res) => {
     const {name, nim, kelas} = req.body;
 
     if (!name || !email || !kelas) {
-        return res.status(400).json({ message : "Nama, nim, dan kelas harus diisi"});
+        return res.status(400).json({ message : "Nama, nim, dan kelas harus diisi."});
     }
 
-   db.query("INSERT INTO users (name, nim, kelas) VALUES (?, ?, ?)", 
-    [name, nim, kelas], 
-    (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ message: "Database Error"});
-        } 
-        res.status(201).json({ message: "User created successfully"});
-    });
+    db.query(
+        "INSERT INTO users (name, nim, kelas) VALUES (?, ?, ?)", 
+        [name, nim, kelas], 
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: "Database Error"});
+            } 
+            res.status(201).json({ message: "User created successfully"});
+        }
+    );
+});
+
+app.put("api/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const { name, nim, kelas } = req.body;
+    db.query(
+        "UPDATE mahasiswa SET name = ?, nim = ?, kelas = ? WHERE id = ?",
+        [name, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: "Database Error" });
+            }
+            res.json({ message: "User updated successfully" });
+        }
+    );
 });
